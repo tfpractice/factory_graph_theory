60686acb387025e40977bb4e7a1074a76b54cf7f
theory friction practice <jackson.brebnor@gmail.com>
2016-07-19 10:20:46 -0400

    e_case graph_theory(major module folder)
    Signed-off-by: theory friction practice <jackson.brebnor@gmail.com>

--- src/graph_theory.js
+++ src/graph_theory.js
@@ -1,15 +1,15 @@
 /**
  * @module GraphTheory
  */
-var Injector = require('./graphTheory/dependency_injector')();
-var Node = require('./graphTheory/node');
-var RobustArray = require('./graphTheory/robust_array_factory');
-var NodeArray = require('./graphTheory/node_array_factory');
-var DirectedEdge = require('./graphTheory/directed_edge');
-var Edge = require('./graphTheory/edge_factory');
-var EdgeArray = require('./graphTheory/edge_array_factory');
-var ComponentArray = require('./graphTheory/component_array_factory');
-var Graph = require('./graphTheory/graph_factory');
+var Injector = require('./graph_theory/dependency_injector')();
+var Node = require('./graph_theory/node');
+var RobustArray = require('./graph_theory/robust_array_factory');
+var NodeArray = require('./graph_theory/node_array_factory');
+var DirectedEdge = require('./graph_theory/directed_edge');
+var Edge = require('./graph_theory/edge_factory');
+var EdgeArray = require('./graph_theory/edge_array_factory');
+var ComponentArray = require('./graph_theory/component_array_factory');
+var Graph = require('./graph_theory/graph_factory');
 
 Injector.register('NodeClass', Node);
 Injector.register('RobustArray', RobustArray);
@@ -20,12 +20,11 @@ Injector.factory('EdgeArray', EdgeArray);
 Injector.factory('Graph', Graph);
 
 Injector.retrieve('Graph');
-exports.NodeArray = Injector.retrieve('NodeArray');
 
 exports.Node = Injector.retrieve("NodeClass");
+exports.NodeArray = Injector.retrieve('NodeArray');
 exports.RobustArray = Injector.retrieve("RobustArray");
 exports.NodeArray = Injector.retrieve("NodeArray");
-// exports.DirectedEdge = Injector.retrieve("DirectedEdge");
 exports.Edge = Injector.retrieve("Edge");
 exports.EdgeArray = Injector.retrieve("EdgeArray");
 exports.ComponentArray = Injector.retrieve("ComponentArray");
